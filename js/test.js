5c49535bc916cd136d3ce0eaa738ee73d8494a84
Eugene Lysov <evg.lysov@gmail.com>
2020-08-24 15:30:36 +0300

    t

--- js/test.js
+++ js/test.js
@@ -1,21 +1,8 @@
-const clients = ['Mango', 'Poly', 'Ajax'];
-const clientNameToFind = 'Polwy';
-let message;
+// split
+const message = 'Welcome to Bahamas!';
 
-for (const client of clients) {
-  /*
-   * На каждой итерации мы будем проверять совпадает ли элемент массива с
-   * именем клиента. Если совпадает то мы записываем в message сообщение
-   * об успехе и делаем break чтобы не искать дальше
-   */
+// Разбивает строку по разделителю, в данном случае это пробел
+console.log(message.split(' ')); // ["Welcome", "to", "Bahamas!"]
 
-  if (client === clientNameToFind) {
-    message = 'Клиент с таким именем есть в базе данных!';
-    break;
-  }
-
-  // Если они не совпадают то запишем в resultMsg сообщeние об отсутсвии имени
-  message = 'Клиента с таким именем нету в базе данных!';
-}
-
-console.log(message); // Клиент с таким именем есть в базе данных!
+// Вызов split с пустой строкой разобьёт по буквам
+console.log(message.split('')); // [много букв :)]
